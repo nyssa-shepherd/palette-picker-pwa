73dd892619f92c0270a2e5a1034ec7a2ecf244e3
Nyssa Keller <nyssa1@gmail.com>
2018-03-12 17:05:55 -0600

    Get random hex values to render within their own box

--- public/js/index.js
+++ public/js/index.js
@@ -11,15 +11,25 @@ const genRandomHex = () => {
   let hex = "#";
   const possible = "ABCDEF0123456789";
 
-  for (var i = 0; i < 6; i++)
-   hex += possible.charAt(Math.floor(Math.random() * possible.length));
-  console.log(hexArr);
-   return makeHexArr(hex);
+  for (var i = 0; i < 6; i++) {
+    hex += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  console.log(hexArr)
+  return randomHexHelper(hex);
 }
 
-const makeHexArr = hex => {
+const randomHexHelper = hex => {
   hexArr.push(hex);
-  hexArr.length < 6 ? genRandomHex() : hexArr = [];
+  hexArr.length < 6 ? genRandomHex() : setColors();
+}
+
+const setColors = () => {
+  $('.box0').text(hexArr[0]);
+  $('.box1').text(hexArr[1]);
+  $('.box2').text(hexArr[2]);
+  $('.box3').text(hexArr[3]);
+  $('.box4').text(hexArr[4]);
+  hexArr = [];
 }
 
 $('.save-project-button').on('click', submitProject);
