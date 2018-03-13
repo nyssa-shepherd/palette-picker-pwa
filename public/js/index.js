d1e63e9c5f71a3ad96f4985bd5763910fe4b6ce1
Nyssa Keller <nyssa1@gmail.com>
2018-03-13 15:02:10 -0600

    Start to buil out functionality for saving palette

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,29 @@
 let hexArr = [];
 const projectArr = [];
+const projObj = {
+  name: '',
+  palName: [],
+  colors: []
+}
 
 const submitProject = e => {
   e.preventDefault();
   let input = $('#project-input').val();
+  projObj.name = input;
 
-  projectArr.push(input);
+  projectArr.push(projObj);
   $('#project-input').val(''); 
   renderProject(input);
-  addProjOptions(input);
 }
 
 const renderProject = (input) => {
   $('.projects').prepend(`<h3 class='proj-name'>${input}</h3>`);
   $('select').prepend(`<option>${input}</option>`);
+}
+
+const callHex = () => {
+  hexArr = [];
+  genRandomHex();
 }
 
 const genRandomHex = () => {
@@ -38,12 +48,28 @@ const setColors = () => {
   $('.box2').text(hexArr[2]).css('background-color', hexArr[2]);
   $('.box3').text(hexArr[3]).css('background-color', hexArr[3]);
   $('.box4').text(hexArr[4]).css('background-color', hexArr[4]);
-  hexArr = [];
+}
+
+const savePalette = (e) => {
+  e.preventDefault();
+  const palObj = {
+    projName: $('select').val(),
+    palName: $('#pal-name-input').val(),
+    colors: hexArr
+  }
+
+  let match = projectArr.find(proj => proj.name === palObj.projName ? proj : null);
+  match.palName.push(palObj.palName);
+  match.colors = palObj.colors;
+  console.log(projectArr);
+  $('#pal-name-input').val('');
 }
 
 $(document).ready(genRandomHex);
+$('.save-pal-btn').on('click', savePalette);
 $('.save-project-button').on('click', submitProject);
-$('.gen-button').on('click', genRandomHex);
+$('.gen-button').on('click', callHex);
+
 
 
 
