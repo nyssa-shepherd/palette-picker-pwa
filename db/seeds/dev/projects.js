3b6f7b89d55d93f92a09b32d468a17d2a337a61d
Nyssa Keller <nyssa1@gmail.com>
2018-03-15 17:15:06 -0600

    Try to get palettes to post to the db but of course having trouble with it

--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
         knex('projects').insert({
           name: 'Nyssa\'s Project'
         }, 'id')
-        .then(paper => {
+        .then(project => {
           return knex('palettes').insert([
             { 
               name: 'Bitchin Blue',
@@ -15,7 +15,7 @@ exports.seed = function(knex, Promise) {
               color2: '#4112e1',
               color3: '#287b95',
               color4: '#0a174c',
-              projects_id: paper[0] },
+              projects_id: project[0]},
             { 
               name: 'Gnarley Green',
               color0: '#154622',
@@ -23,7 +23,7 @@ exports.seed = function(knex, Promise) {
               color2: '#45de03',
               color3: '#63790c',
               color4: '#3a7e4d',
-              projects_id: paper[0] }
+              projects_id: project[0] }
           ])
         })
         .then(() => console.log('Seeding complete!'))
--- server.js
+++ server.js
@@ -40,12 +40,59 @@ app.post('/api/v1/projects', (request, response) => {
     });
 });
 
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+    .then((palettes) => {
+      response.status(200).json(palettes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
 app.get('/api/v1/projects/:id/palettes', (request, response) => {
   database('palettes').select()
     .then((palettes) => {
       response.status(200).json(palettes);
     })
     .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/projects/:id/palettes', (request, response) => {
+  const palettes = request.body;
+
+  for (let requiredParameter of ['name', 'color0', 'color1', 'color2', 'color3', 'color4', 'projects_id']) {
+    if (!palettes[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { 
+          name: <String>, 
+          color0: <String>, 
+          color1: <String>, 
+          color2: <String>, 
+          color3: <String>, 
+          color4: <String>,
+          projects_id: <Number>}. You're missing a "${requiredParameter}" property.` 
+        });
+    }
+  }
+
+  database('palettes').insert(palettes, 'id')
+    .then(palettes => {
+      response.status(201).json({ 
+        id: palettes_id[0], 
+        name: palettes.name,
+        color0: palettes.color0, 
+        color1: palettes.color1, 
+        color2: palettes.color2, 
+        color3: palettes.color3, 
+        color4: palettes.color4,
+        projects_id: palettes.projMatch.id
+       })
+    })
+    .catch(error => {
       response.status(500).json({ error });
     });
 });
