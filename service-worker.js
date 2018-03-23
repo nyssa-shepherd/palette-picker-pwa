f140e7698f7ee9832649e9996146108ab7235701
Nyssa Keller <nyssa1@gmail.com>
2018-03-22 21:01:10 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+
+this.addEventListener('install', event => {
+  event.waitUntil();
+});
+
+this.addEventListener('fetch', event => {
+  event.respondWith();
+});
+
+this.addEventListener('activate', (event) => {
+
+  event.waitUntil();
+});
